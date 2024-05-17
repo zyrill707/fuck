e54ad9f0f9e3e9be780f618283e0cef76cc002b2
testproject-testing-cf <56145975+zyrill707@users.noreply.github.com>
2024-05-17 14:52:28 +0800

    Update zyrill.js

--- zyrill.js
+++ zyrill.js
@@ -2,23 +2,23 @@ document.getElementById("dl").onclick = function(){
       var dlVid = document.createElement("a");
         dlVid.href = "vid1.mp4"
       dlVid.download = "Video.mp4"
-      dlVid.click()
+      dlVid.click();
     }
     document.getElementById("dl1").onclick = function(){
       var dlVid1 = document.createElement("a");
       dlVid1.href = "vid2.mp4";
       dlVid1.download = "mahal kita.mp4"
-      dlVid1.click()
+      dlVid1.click();
     }
     document.getElementById("dl2").onclick = function(){
       var dlVid2 = document.createElement("a");
       dlVid2.href = "vid3.mp4";
       dlVid2.download = "Ikaw lang sapat na.mp4";
-      dlVid2.click()
+      dlVid2.click();
     }
     document.getElementById("last").onclick = function(){
       var lastVid = document.createElement('a');
       lastVid.href = "vid4.mp4";
       lastVid.download = " Hi Baby I'm sorry sa pinagagawa ko Mahal na Mahal kita sana pumayag kana sa kasal akin kalang ha kahit medyo abno ka ikaw lang baby sayo lang napakaswerte ko sayo ina mo ka sobrang ganda mo sexy pa uy mwaaaa labyouuuuu.mp4";
-      lastVid.click()
+      lastVid.click();
     }
